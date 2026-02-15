9729b35808f735b175c93519ef1f1c57162675b5
baptiste-be <bertrandbaptiste13@gmail.com>
2026-02-15 20:05:02 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCJZ7do_9CSXrG2npc1DPi0F2mzmRUFuBw",
+  authDomain: "phone-7b65b.firebaseapp.com",
+  projectId: "phone-7b65b",
+  storageBucket: "phone-7b65b.firebasestorage.app",
+  messagingSenderId: "385403300521",
+  appId: "1:385403300521:web:7e9c475a025fce7b8697b8"
 };
